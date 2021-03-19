4bce5d2deeb9d4ecbbef9d38792624550db8cd06
Miftahul Arifin <miftahul97@gmail.com>
2021-03-19 07:28:11 +0700

    add element and apexchart

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import VueApexCharts from "vue3-apexcharts";
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueApexCharts);
+app.use(ElementPlus);
+
+app.mount('#app');
